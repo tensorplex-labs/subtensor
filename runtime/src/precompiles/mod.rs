0308f7e30179c82ae48f6fa5d447318597f2325d
Greg Zaitsev <gregz@opentensor.dev>
2024-08-27 13:42:47 -0400

    Add SubtensorBalanceTransfer precompile

--- runtime/src/precompiles/mod.rs
+++ runtime/src/precompiles/mod.rs
@@ -1,12 +1,21 @@
 use core::marker::PhantomData;
-use pallet_evm::{
-	IsPrecompileResult, Precompile, PrecompileHandle, PrecompileResult, PrecompileSet,
+use sp_core::{
+	crypto::ByteArray, hashing::keccak_256, H160
 };
-use sp_core::H160;
+use sp_runtime::AccountId32;
 
+use pallet_evm::{
+	ExitError, IsPrecompileResult, Precompile, PrecompileHandle, PrecompileResult, PrecompileSet,
+	PrecompileFailure
+};
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
+
+// Include custom precompiles
+mod balance_transfer;
+
+use balance_transfer::*;
 
 pub struct FrontierPrecompiles<R>(PhantomData<R>);
 
@@ -17,7 +26,7 @@ where
 	pub fn new() -> Self {
 		Self(Default::default())
 	}
-	pub fn used_addresses() -> [H160; 7] {
+	pub fn used_addresses() -> [H160; 8] {
 		[
 			hash(1),
 			hash(2),
@@ -26,6 +35,7 @@ where
 			hash(5),
 			hash(1024),
 			hash(1025),
+			hash(BALANCE_TRANSFER_INDEX)
 		]
 	}
 }
@@ -44,6 +54,7 @@ where
 			// Non-Frontier specific nor Ethereum precompiles :
 			a if a == hash(1024) => Some(Sha3FIPS256::execute(handle)),
 			a if a == hash(1025) => Some(ECRecoverPublicKey::execute(handle)),
+			a if a == hash(BALANCE_TRANSFER_INDEX) => Some(BalanceTransferPrecompile::execute(handle)),
 			_ => None,
 		}
 	}
@@ -58,4 +69,33 @@ where
 
 fn hash(a: u64) -> H160 {
 	H160::from_low_u64_be(a)
+}
+
+/// Returns Ethereum method ID from an str method signature
+/// 
+pub fn get_method_id(method_signature: &str) -> [u8; 4] {
+    // Calculate the full Keccak-256 hash of the method signature
+    let hash = keccak_256(method_signature.as_bytes());
+
+    // Extract the first 4 bytes to get the method ID
+    let method_id = [
+        hash[0],
+        hash[1],
+        hash[2],
+        hash[3],
+    ];
+
+    method_id
+}
+
+/// Convert bytes to AccountId32 with PrecompileFailure as Error
+/// which consumes all gas
+/// 
+pub fn bytes_to_account_id(account_id_bytes: &[u8]) -> Result<AccountId32, PrecompileFailure> {
+    AccountId32::from_slice(&account_id_bytes).map_err(|_| {
+        log::info!("Error parsing account id bytes {:?}", account_id_bytes);
+		PrecompileFailure::Error {
+			exit_status: ExitError::InvalidRange
+		}
+    })
 }
